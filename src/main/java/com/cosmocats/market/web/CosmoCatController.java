d699b45c5eaabfc50b43d831b795c08d273b8a6f
Andrii <savchenko.andrii05@gmail.com>
2025-12-07 12:38:20 +0200

    fixed your comments

--- src/main/java/com/cosmocats/market/web/CosmoCatController.java
+++ src/main/java/com/cosmocats/market/web/CosmoCatController.java
@@ -2,11 +2,13 @@ package com.cosmocats.market.web;
 
 import com.cosmocats.market.service.CosmoCatService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/v1") // add API versioning
 public class CosmoCatController {
 
     private final CosmoCatService service;
@@ -15,7 +17,7 @@ public class CosmoCatController {
         this.service = service;
     }
 
-    @GetMapping("/api/cosmo-cats")
+    @GetMapping("/cosmo-cats")
     public List<String> getCosmoCats() {
         return service.getCosmoCats();
     }
--- src/test/java/com/cosmocats/market/it/CosmoCatControllerIT.java
+++ src/test/java/com/cosmocats/market/it/CosmoCatControllerIT.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -12,7 +11,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@TestPropertySource(properties = "feature.cosmoCats.enabled=true")
 class CosmoCatControllerIT {
 
     @Autowired
@@ -20,7 +18,7 @@ class CosmoCatControllerIT {
 
     @Test
     void getCosmoCats_enabled_returns200() throws Exception {
-        mvc.perform(get("/api/cosmo-cats"))
+        mvc.perform(get("/api/v1/cosmo-cats")) //versioning
                 .andExpect(status().isOk());
     }
 }
