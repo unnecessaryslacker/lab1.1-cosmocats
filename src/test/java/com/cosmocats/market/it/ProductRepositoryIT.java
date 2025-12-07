3c4fba3191c72a2ae43f1c3645a99bbd49a3975b
Andrii <savchenko.andrii05@gmail.com>
2025-12-07 18:10:14 +0200

    fixed all god damn errors

--- src/test/java/com/cosmocats/market/it/ProductRepositoryIT.java
+++ src/test/java/com/cosmocats/market/it/ProductRepositoryIT.java
@@ -35,6 +35,12 @@ class ProductRepositoryIT {
         registry.add("spring.datasource.url", postgres::getJdbcUrl);
         registry.add("spring.datasource.username", postgres::getUsername);
         registry.add("spring.datasource.password", postgres::getPassword);
+
+        registry.add("spring.datasource.driver-class-name", () -> "org.postgresql.Driver");
+        registry.add("spring.jpa.properties.hibernate.dialect",
+                () -> "org.hibernate.dialect.PostgreSQLDialect");
+
+        registry.add("spring.liquibase.enabled", () -> "true");
     }
 
     @Autowired
